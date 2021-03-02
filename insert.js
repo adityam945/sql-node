89e1598bb398989f3b009d7884d60dc295277917
adityam945 <adityam945@gmail.com>
2021-03-02 13:16:56 +0530

    initial commiit

---
+++ insert.js
@@ -0,0 +1,53 @@
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "sql_node_conn1",
+});
+
+//
+
+// one record
+
+//
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql =
+//     "INSERT INTO customers (name, address) VALUES ('Company ABC', 'Addr 123')";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("1 record inserted");
+//   });
+// });
+
+//
+//
+//
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO customers (name, address) VALUES ?";
+  var values = [
+    ["John", "Highway 71"],
+    ["Peter", "Lowstreet 4"],
+    ["Amy", "Apple st 652"],
+    ["Hannah", "Mountain 21"],
+    ["Michael", "Valley 345"],
+    ["Sandy", "Ocean blvd 2"],
+    ["Betty", "Green Grass 1"],
+    ["Richard", "Sky st 331"],
+    ["Susan", "One way 98"],
+    ["Vicky", "Yellow Garden 2"],
+    ["Ben", "Park Lane 38"],
+    ["William", "Central st 954"],
+    ["Chuck", "Main Road 989"],
+    ["Viola", "Sideway 1633"],
+  ];
+  con.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+});
---
+++ query.js
@@ -0,0 +1,19 @@
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "sql_node_conn1",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  con.query(
+    "SELECT * FROM customers WHERE address = 'Park Lane 38'",
+    function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    }
+  );
+});
